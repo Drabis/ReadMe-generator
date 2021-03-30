37d15c689f313aa4573dc86737749f55e362ca72
Drabis <drabis47@gmail.com>
2021-03-29 22:27:33 -0400

    utils file

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,49 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## Table of Contents
+  *[Project title](#title)
+  *[Description](description)
+  *[Contribution](# contribution)
+  *[License](#lincense)
+  *[Username](#username)
+  *[Email](#email)
+
+  ## Project title
+  ${data.description}
+
+  ##Description
+  ${data.installation}
+  ##Contribution
+  ${data.contribution}
+
+  ## License
+  ${data.license}
+
+  ##Username
+  ${data.username}
+
+  #Email
+  ${data.email}
+
+  ## Questions
+  constact me:
+  Github: [${data.username}](github.com/Drabis)
+  Email: [${data.email}](drabo.aboulaye88@gmail.com)
+
+`;
+}
+
+module.exports = generateMarkdown;
