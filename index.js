d8c3ca91176b3177155e8753f269f834aed0a213
Drabis <drabis47@gmail.com>
2021-04-01 18:18:50 -0400

    update the index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -40,18 +42,16 @@ const questions = [
 	},
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    writeToFile(fileName, data, err => {
-        err ? console.log(err) : console.log('Readme created!')
-        
-    })
-}
-
 // TODO: Create a function to initialize app
-function init() {
-    prompt(questions).then
-}
+function init(){
+// TODO: Create a function to write README file
+inquirer.prompt(questions).then((data) => {
+    console.log(data);
 
+    fs.writeToFile('README.md', generateMarkdown(data), (err) => 
+        err ? console.log(err) : console.log('Readme created!')
+    );   
+});
+}
 // Function call to initialize app
 init();
